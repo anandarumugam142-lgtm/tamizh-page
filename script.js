960be83e6502eaf26d2eb30b2d1cf052da0b4a27
anandarumugam142-lgtm <anandarumugam142@gmail.com>
2026-03-09 18:01:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function check(answer){
+
+if(answer=="water"){
+document.getElementById("result").innerHTML="Correct!"
+}
+else{
+document.getElementById("result").innerHTML="Wrong Answer"
+}
+
+}
